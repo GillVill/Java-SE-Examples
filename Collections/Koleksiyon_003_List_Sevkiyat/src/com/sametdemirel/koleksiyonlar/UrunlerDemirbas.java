6f9a21933ad65eb251f6967dc98941cb7720919c
GillVill <gillvill.sd@gmail.com>
2018-11-07 03:50:39 +0300

    Remove Collections Directory

--- Collections/Koleksiyon_003_List_Sevkiyat/src/com/sametdemirel/koleksiyonlar/UrunlerDemirbas.java
+++
@@ -1,8 +0,0 @@
-package com.sametdemirel.koleksiyonlar;
-
-public class UrunlerDemirbas {
-
-    public static Urunler kapi = new Urunler("Kapı",35);
-    public static Urunler parke = new Urunler("Laminat Parke", 25);
-    public static Urunler pencere = new Urunler("Cam Pencere",10);
-}
