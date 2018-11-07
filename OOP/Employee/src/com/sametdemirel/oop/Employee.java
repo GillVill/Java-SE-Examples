b175aa395ee9b728d2757c4b4695355aa19ddfb1
GillVill <gillvill.sd@gmail.com>
2018-11-07 18:20:30 +0300

    Object Oriented Programming

---
+++ OOP/Employee/src/com/sametdemirel/oop/Employee.java
@@ -0,0 +1,47 @@
+package com.sametdemirel.oop;
+
+public class Employee {
+
+    private String name;
+    private int age;
+    private String designation;
+    private int salary;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", designation='" + designation + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
